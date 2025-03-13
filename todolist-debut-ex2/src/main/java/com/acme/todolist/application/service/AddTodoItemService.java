58d354ba5c49eb3f2893fc6b53c6a10b1b0fdbc8
Baptiste <baptiste@generation-internet.biz>
2025-03-13 14:21:35 +0100

    debut ex2

--- todolist-debut-ex2/src/main/java/com/acme/todolist/application/service/AddTodoItemService.java
+++ todolist-debut-ex2/src/main/java/com/acme/todolist/application/service/AddTodoItemService.java
@@ -2,21 +2,37 @@ package com.acme.todolist.application.service;
 
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
+import org.springframework.web.bind.annotation.*;
 import com.acme.todolist.application.port.in.AddTodoItem;
 import com.acme.todolist.application.port.out.UpdateTodoItem;
 import com.acme.todolist.domain.TodoItem;
 
 @Component
+@RestController
+@RequestMapping("/todos")
 public class AddTodoItemService implements AddTodoItem {
 
-	private UpdateTodoItem updateTodoItem;
+	private final UpdateTodoItem updateTodoItem;
 
-	//A compléter
+	@Autowired
 	public AddTodoItemService(UpdateTodoItem updateTodoItem) {
+		this.updateTodoItem = updateTodoItem;
 	}
 
-	//A compléter	
+	@Override
+	public void addTodoItem(TodoItem item) {
+		if (item == null) {
+			throw new IllegalArgumentException("TodoItem ne doit pas être null");
+		}
+		updateTodoItem.storeNewTodoItem(item);
+	}
 
+	@PostMapping
+	public ResponseEntity<Void> createTodoItem(@RequestBody TodoItem item) {
+		addTodoItem(item);
+		return ResponseEntity.status(201).build();
+	}
 }
